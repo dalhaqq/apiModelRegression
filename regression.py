926316f6783bfb4ad96669d022196620758f9b52
Narotama Basundara <narotamaaa3@gmail.com>
2024-06-03 21:22:11 +0700

    fix

--- regression.py
+++ regression.py
@@ -1,26 +1,10 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-
-import warnings # supress warnings
-warnings.filterwarnings('ignore')
-
-df = pd.read_csv('./new-dataset.csv')
-
-df = df.drop_duplicates(keep='first')
-
-x = df.drop(columns=['is_color','bw_area','price'])
-y = df['price']
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
-
-lin_reg = LinearRegression()
-lin_reg.fit(x_train, y_train)
+def lin_reg(x1, x2):
+    return 191.3642 + (x1 * 19.59733806) + (x2 * 7.05360083)
 
 def pricePrediction(color_area, print_area):
-    prediction = lin_reg.predict([[color_area, print_area]])
-    if prediction[0] < 300 and color_area == 0 :
-        prediction[0] = 300
-    if prediction[0] < 500 and color_area != 0 :
-        prediction[0] = 500
-    return (round(prediction[0]))
+    prediction = lin_reg(color_area, print_area)
+    if prediction < 300 and color_area == 0 :
+        prediction = 300
+    if prediction < 500 and color_area != 0 :
+        prediction = 500
+    return (round(prediction))
